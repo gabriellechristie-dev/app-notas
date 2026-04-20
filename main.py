46d4a5fda5ab77acc606465050b4080f367a1bc1
Christie <gabriellechristie000@gmail.com>
2026-04-20 20:01:21 -0300

    refactor : organização em funções

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
-lista_de_notas = []
+def criar_nota():
+    lista_de_notas = []
+    print("Digite 'esc' para sair")
+    
+    while True:
+        nota = (input("Digite sua anotação"))
+        if nota == "esc":
+            break
+        else:
+           lista_de_notas.append(nota)
+    return lista_de_notas
 
-while True:
-    print("Digite 'esc' para sair!")
-    nota = (input("Digite uma nova nota..."))
-    if nota == "esc":
-        break
-    elif nota == "":
-        print("Não é possível criar uma  nota vazia")
-        continue
-    lista_de_notas.append(nota)
-if not lista_de_notas:
-    print("Nenhuma nota cadastrada!")
-else:
-    for indice, valor in enumerate(lista_de_notas):
-        print(f"{indice + 1} - {valor}")
+def editar_nota(lista_de_notas):
+    edicao = (input("Deseja editar uma nota? s/n"))
+    if edicao == "s":
+        print(lista_de_notas)
+        num_usuario = (int(input("Digite o numero da nota")))
+        if num_usuario >= 1 and num_usuario <= len(lista_de_notas):
+            num_usuario -= 1
+            nova_nota = (input("Digite sua edição"))
+            lista_de_notas[num_usuario] = nova_nota
+  
+    return lista_de_notas
 
-if not lista_de_notas:
-    print("Nenhuma nota para remover!")
-else:
-    for indice, valor in enumerate(lista_de_notas):
-        print(f"{indice + 1} - {valor}")
-   
+
+    
