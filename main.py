45620ae82eea904778ffdf5a9a8784d2effe08fd
Christie <gabriellechristie000@gmail.com>
2026-04-22 18:33:23 -0300

    feat : add excluir nota

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
 def criar_nota():
     lista_de_notas = []
-    print("Digite 'esc' para sair")
     
     while True:
-        nota = (input("Digite sua anotação"))
-        if nota == "esc":
-            break
-        else:
-           lista_de_notas.append(nota)
-    return lista_de_notas
+        nota = (input("Digite sua anotação: "))
+        if nota:
+         lista_de_notas.append(nota)
+           
+        return lista_de_notas
 
 def editar_nota(lista_de_notas):
     edicao = (input("Deseja editar uma nota? s/n"))
     if edicao == "s":
-        print(lista_de_notas)
+        for indice, valor in enumerate(lista_de_notas):
+            print(indice + 1, "-", valor )
         num_usuario = (int(input("Digite o numero da nota")))
         if num_usuario >= 1 and num_usuario <= len(lista_de_notas):
             num_usuario -= 1
@@ -21,6 +20,22 @@ def editar_nota(lista_de_notas):
             lista_de_notas[num_usuario] = nova_nota
   
     return lista_de_notas
+
+def excluir_nota(lista_de_notas):
+    excluir = (input("Deseja excluir uma nota? s/n"))
+    if excluir == "s":
+        for indice, valor in enumerate(lista_de_notas):
+            print(indice + 1, "-", valor )
+        n_usuario = (int(input("Digite o número da nota ")))
+        if n_usuario >= 1 and n_usuario <= len(lista_de_notas):
+            n_usuario -= 1
+            lista_de_notas.pop(n_usuario)
+
+    return lista_de_notas
+
+lista_de_notas = criar_nota()
+lista_de_notas = editar_nota(lista_de_notas)
+lista_de_notas = excluir_nota(lista_de_notas)
 
 
     
